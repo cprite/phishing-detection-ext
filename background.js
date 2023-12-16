f0a560383f70c9640343f11a1451fc5162e0b226
Nikolay <98195072+cprite@users.noreply.github.com>
2023-12-16 18:35:11 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,20 +1,16 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete' && tab.url && !tab.url.startsWith('chrome://')) {
-    chrome.storage.sync.get('isEnabled', function(data) {
-      if (data.isEnabled) {
-        fetch('http://127.0.0.1:5000/check_url', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ url: tab.url })
-        })
-        .then(response => response.json())
-        .then(data => {
-          if (data.decision === 'PHISHING') {
-            chrome.tabs.update(tabId, { url: "warning.html" });
-          }
-        })
-        .catch(error => console.error('Error:', error));
+    fetch('http://127.0.0.1:5000/check_url', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ url: tab.url })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.decision === 'PHISHING') {
+        chrome.tabs.update(tabId, { url: "warning.html" });
       }
-    });
+    })
+    .catch(error => console.error('Error:', error));
   }
 });
